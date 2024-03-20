d3202d1da8c2e360564c2c85b061d938c1648b20
rajul786dixit <rajul786dixit@gmail.com>
2024-03-20 19:17:02 +0100

    token prices method added

--- dex-server/app.js
+++ dex-server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const Moralis = require("moralis").default;
 const cors = require("cors");
-
+require("dotenv").config();
 var app = express();
 
 // view engine setup
@@ -19,7 +19,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 const API_KEY =
+  process.env.MORALIS_KEY ||
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjIwOTNiOWY4LTJlZWYtNGU4NS1iMTIwLWU1MTMxNmU0NjJkZCIsIm9yZ0lkIjoiMzgzNjUzIiwidXNlcklkIjoiMzk0MjE0IiwidHlwZUlkIjoiZjgyNmI0NDMtMDgwOC00ZGU1LWIzYjYtNWQ5ZDBiZjY0ZTI5IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MTA4NjA3MjUsImV4cCI6NDg2NjYyMDcyNX0.5JUFoYkcSCWiP79tHvXAkXxLnDHaC03VkW3K4eN47gc";
+
+app.get("/tokenPrices", async (req, res) => {
+  await Moralis.start({
+    apiKey: API_KEY
+  });
+  const { query } = req;
+  try {
+    const respOne = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressOne
+    });
+
+    const respTwo = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressTwo
+    });
+
+    const usdPrices = {
+      tokenOne: respOne.raw.usdPrice,
+      tokenTwo: respTwo.raw.usdPrice,
+      ratio: respOne.raw.usdPrice / respTwo.raw.usdPrice
+    };
+
+    return res.status(200).json(usdPrices);
+  } catch (error) {
+    return res.status(500).json({});
+  }
+});
 app.get("/tokenList", async (req, res) => {
   try {
     const address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
@@ -27,7 +54,6 @@ app.get("/tokenList", async (req, res) => {
     await Moralis.start({
       apiKey: API_KEY
     });
-    console.log("key :::", API_KEY);
     const response = await Moralis.EvmApi.token.getTokenPrice({
       address: address
     });
